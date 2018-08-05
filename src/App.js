1cbddd44db8f9011688569252964b1130f16c3c0
Laurianne Terrier <laurianne.terrier@yahoo.fr>
2018-08-05 19:51:25 +0200

    Set up react and add first typography styles

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Navbar from './components/Navbar/Navbar';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1 className="heading-1">Ceci est un titre de niveau 1 </h1>
+        <h2 className="heading-2">Ceci est un titre de niveau 2 </h2>
+        <h3 className="heading-3">Ceci est un titre de niveau 3 </h3>
+        <h4 className="heading-4">Ceci est un titre de niveau 4 </h4>
+        <h5 className="heading-5">Ceci est un titre de niveau 5 </h5>
+        <h6 className="heading-6">Ceci est un titre de niveau 6 </h6>
+        <p className="text">Ceci est du texte tout simple</p>
+        <p className="text">Ceci est du texte tout simple dans un paragraphe, juste pour voir un peu. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam placerat vestibulum sem. Etiam id dapibus arcu, non malesuada leo. Phasellus auctor arcu eu turpis pharetra molestie. Nam libero nibh, ornare ut velit at, aliquam ultrices augue. Quisque rhoncus lacus feugiat consectetur luctus. Curabitur rhoncus scelerisque nulla non pretium. Ut in odio non tellus euismod dapibus in vel risus. Nullam fringilla augue ornare tortor cursus, sed consectetur mi tincidunt. Sed in risus eget purus condimentum rutrum.</p>
+        <a href="#" className="link">Ceci est un lien </a>
+        <a href="#" className="link is-active">Ceci est un lien actif</a>
+        <Navbar />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Navbar.css';
+
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div className="Navbar">
+        <h1>Ceci est une navbar!</h1>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
