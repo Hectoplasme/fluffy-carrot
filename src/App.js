27a4efaf80469078bab0aed86c8fcd7338946237
Laurianne <laurianne@spintank.fr>
2018-08-13 17:46:50 +0200

    Add masonry layout to the recipe list

--- src/App.js
+++ src/App.js
@@ -10,23 +10,7 @@ class App extends Component {
       <div className="App">
         <Navbar />
         <RecipeList />
-        <h1 className="heading-1">Ceci est un titre de niveau 1 </h1>
-        <h2 className="heading-2">Ceci est un titre de niveau 2 </h2>
-        <h3 className="heading-3">Ceci est un titre de niveau 3 </h3>
-        <h4 className="heading-4">Ceci est un titre de niveau 4 </h4>
-        <h5 className="heading-5">Ceci est un titre de niveau 5 </h5>
-        <h6 className="heading-6">Ceci est un titre de niveau 6 </h6>
-        <p className="text">Ceci est du texte tout simple</p>
-        <p className="text">Ceci est du texte tout simple dans un paragraphe, juste pour voir un peu. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam placerat vestibulum sem. Etiam id dapibus arcu, non malesuada leo. Phasellus auctor arcu eu turpis pharetra molestie. Nam libero nibh, ornare ut velit at, aliquam ultrices augue. Quisque rhoncus lacus feugiat consectetur luctus. Curabitur rhoncus scelerisque nulla non pretium. Ut in odio non tellus euismod dapibus in vel risus. Nullam fringilla augue ornare tortor cursus, sed consectetur mi tincidunt. Sed in risus eget purus condimentum rutrum.</p>
-        <a href="#" className="link">Ceci est un lien </a>
-        <a href="#" className="link"><i className="icon fas fa-user-circle"></i>Ceci est un lien avec un icone</a>
-        <a href="#" className="link is-active">Ceci est un lien actif</a>
-        <button className="link">Pouet</button>
-        <p className="text"><button className="btn btn-floating"><i className="icon fas fa-plus"></i></button><button className="btn btn-floating"><i className="icon fas fa-plus-circle icon-accent icon-large"></i></button></p>
-        <div className="input-field input-field--search">
-          <i className="icon fas fa-search"></i>
-          <input autoComplete="off" placeholder="Rechercher" className="input" />
-        </div>
+        
       </div>
     );
   }
--- src/components/RecipeList/RecipeList.js
+++ src/components/RecipeList/RecipeList.js
@@ -2,21 +2,39 @@ import React, { Component } from 'react';
 import './RecipeList.css';
 import RecipeCard from './RecipeCard/RecipeCard';
 
+import Masonry from 'react-masonry-css';
+
 
 class RecipeList extends Component {
-  render() {
-    return (
-        <div className={`${this.props.className || ""} card-list`}>
-            <RecipeCard />
-            <RecipeCard />
-            <RecipeCard />
-            <RecipeCard />
-            <RecipeCard />
-            <RecipeCard />
-            <RecipeCard />
-        </div>
-    );
-  }
+	render() {
+		const breakpointColumnsObj = {
+			default : 5,
+			850 : 4,
+			500 : 3,
+			350 : 2
+		};
+		return (
+				<Masonry 
+					breakpointCols={breakpointColumnsObj} 
+					className={`${this.props.className || ""} card-list`}
+					columnClassName="card-column">
+						<RecipeCard />
+						<RecipeCard />
+						<RecipeCard />
+						<RecipeCard />
+						<RecipeCard />
+						<RecipeCard />
+						<RecipeCard />
+						<RecipeCard />
+						<RecipeCard />
+						<RecipeCard />
+						<RecipeCard />
+						<RecipeCard />
+						<RecipeCard />
+						<RecipeCard />
+				</Masonry>
+		);
+	}
 }
 
 export default RecipeList;
