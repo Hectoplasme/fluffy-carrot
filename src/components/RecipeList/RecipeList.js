b214624609c170e1a783e246e011168ba7171566
Laurianne <laurianne@spintank.fr>
2018-08-13 18:02:55 +0200

    Fix some breakpoint in the recipe layout and add the container class

--- src/components/RecipeList/RecipeList.js
+++ src/components/RecipeList/RecipeList.js
@@ -8,12 +8,15 @@ import Masonry from 'react-masonry-css';
 class RecipeList extends Component {
 	render() {
 		const breakpointColumnsObj = {
-			default : 5,
-			850 : 4,
-			500 : 3,
-			350 : 2
+			default : 6,
+			1600: 5,
+			1500 : 4,
+			1200 : 3,
+			850 : 2,
+			500: 1
 		};
 		return (
+			<div class="container">
 				<Masonry 
 					breakpointCols={breakpointColumnsObj} 
 					className={`${this.props.className || ""} card-list`}
@@ -33,6 +36,7 @@ class RecipeList extends Component {
 						<RecipeCard />
 						<RecipeCard />
 				</Masonry>
+			</div>
 		);
 	}
 }
