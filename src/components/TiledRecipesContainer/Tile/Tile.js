dcb48efac6f578fb425e1240da171f795e609bba
Laurianne <laurianne@spintank.fr>
2018-08-14 15:09:27 +0200

    Create the Tile component

---
+++ src/components/TiledRecipesContainer/Tile/Tile.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+//CSS
+import './Tile.css'
+
+class Tile extends Component {
+  render() {
+    return (
+      <div className="tile">
+      </div>
+    );
+  }
+}
+
+export default Tile;
