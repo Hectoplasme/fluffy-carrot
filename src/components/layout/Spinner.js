63b0d791499bbbd359c3b50b158d5f36cbb5e4b5
Laurianne <laurianne@spintank.fr>
2018-09-11 22:18:06 +0200

    Configure react redux firebase

---
+++ src/components/layout/Spinner.js
@@ -0,0 +1,10 @@
+import React from "react";
+import spinner from "./spinner.gif";
+
+export default () => {
+  return (
+    <div className="max-w-2xs mx-auto p-32 text-center">
+      <i className="text-3xl text-grey fas fa-spinner fa-spin" />
+    </div>
+  );
+};
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,53 +1,39 @@
 import React, { Component } from "react";
-import Recipes from "../recipes/Recipes";
+import PropTypes from "prop-types";
 
-//Data placeholder
-const recipes = [
-  {
-    id: "recipe-1",
-    title: "Pâtes à la carbonara",
-    imgUrl: "https://picsum.photos/200/300/?random",
-    height: 300,
-    width: 200
-  },
-  {
-    id: "recipe-2",
-    title: "Pâtes bolognaise",
-    imgUrl: "https://picsum.photos/500/500/?random",
-    height: 500,
-    width: 500
-  },
-  {
-    id: "recipe-3",
-    title: "Risotto au poulet",
-    imgUrl: "https://picsum.photos/300/400/?random",
-    height: 400,
-    width: 300
-  },
-  {
-    id: "recipe-4",
-    title: "Risotto aux champignons",
-    imgUrl: "https://picsum.photos/200/400/?random",
-    height: 400,
-    width: 200
-  },
-  {
-    id: "recipe-5",
-    title: "Pâtes à la carbonara",
-    imgUrl: "https://picsum.photos/200/300/?random",
-    height: 300,
-    width: 200
-  }
-];
+//Redux
+import { compose } from "redux";
+import { connect } from "react-redux";
+import { firestoreConnect } from "react-redux-firebase";
+
+//Component
+import Recipes from "../recipes/Recipes";
+import Spinner from "../layout/Spinner";
 
 class Home extends Component {
   render() {
-    return (
-      <div>
-        <Recipes recipes={recipes} />
-      </div>
-    );
+    const { recipes } = this.props;
+
+    if (recipes) {
+      return (
+        <div>
+          <Recipes recipes={recipes} />
+        </div>
+      );
+    } else {
+      return <Spinner />;
+    }
   }
 }
 
-export default Home;
+Home.propTypes = {
+  firestore: PropTypes.object.isRequired,
+  recipes: PropTypes.array
+};
+
+export default compose(
+  firestoreConnect([{ collection: "recipes" }]),
+  connect((state, props) => ({
+    recipes: state.firestore.ordered.recipes
+  }))
+)(Home);
